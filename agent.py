0e7e0b4e313af40c34f6d5836d2ddd7fdad137a3
cbertugl <camille.bertuglia@insa-cvl.fr>
2018-10-01 10:20:49 -0400

    first commit

---
+++ agent.py
@@ -0,0 +1,66 @@
+
+class Agent:
+    
+    def __init__(self,x,y):
+        
+        self.posx = x #position du robot sur l'axe x
+        self.posy = y #position du robot sur l'axe y
+        self.dbag_dirt = 0 #nombre de poussiere dans le sac a poussiere
+        self.dbag_jewel = 0 #nombre de bijoux dans le sac a poussiere
+        self.jbag = 0 #nombre de bijoux dans le sac a bijou
+        self.energy = 0 #energie consommee
+
+    def aspire(self,e): #aspire le contenu de la piece dans le sac a poussiere
+        
+        if e.hmap[self.posx][self.posy] = 1:
+            self.dbag_dirt = self.dbag_dirt + 1
+        
+        elif e.hmap[self.posx][self.posy] = 2:
+            self.dbag_jewel = self.dbag_jewel + 1
+        
+        elif e.hmap[self.posx][self.posy] =3:
+            self.dbag_dirt = self.dbag_dirt + 1
+            self.dbag_jewel = self.dbag_jewel + 1
+        
+        #todo: mettre a jour l'etat de la piece
+        self.energy = self.energy +1
+        
+        
+    def grab(self,e): #ramasse le contenu de la piece dans le sac a bijou
+
+        if e.hmap[self.posx][self.posy] =2 || e.hmap[self.posx][self.posy] =3:
+            self.jbag = self.jbag + 1
+        #todo: mettre a jour l'etat de la piece
+            
+        self.energy = self.energy + 1
+        
+        
+    def move(self,direction,e): #se deplace dans la direction voulue
+        
+        if direction = up && self.posy < len(e.y):
+            self.posy = self.posy + 1
+            
+        elif direction = down && self.posy > 0:
+            self.posy = self.posy - 1
+        
+        elif direction = left && self.posx > 0:
+            self.posy = self.posx + 1
+        
+        elif direction = right && self.posx < len(e.x):
+            self.posy = self.posx - 1
+            
+        else:
+            print("Invalid moving order")
+        
+        self.energy = self.energy + 1
+        
+        
+class Captor:
+    #todo
+    
+class Effector:
+    #todo
+    
+    
+
+        
---
+++ environment.py
@@ -0,0 +1,14 @@
+class Environment:
+    
+    def __init__(self):
+        self.x, self.y = 10, 10; #cree un tableau 2D de 10 par 10
+        self.hmap= [[0 for i in range(y)] for j in range(y)] #initialise le tableau a 0
+        #on considere 0 = rien, 1 = poussiere, 2 = bijou, 3 = poussiere et bijou
+        
+    def gen:
+    
+    def gendust:
+        
+    def genjewel:
+        
+        
