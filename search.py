840d3037962f6689795e38d2ed4d0073c843c9ff
cbertugl <camille.bertuglia@insa-cvl.fr>
2018-10-04 10:02:09 -0400

    ajout d'une todolilst, ajoutez vos idées et mettez vos noms à coté des tâches que vous êtes en train de faire

--- search.py
+++ search.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python
 
-def breadth_first_search(starting_room):
+def breadth_first_search(starting_room, goal_room):
     path = [] #chemin parcouru
     queue = [starting_room] #file à explorer
     while not queue.empty(): #tant qu'il nous reste quelque chose à explorer
         current_room = queue.pop()
         path.append(current_room)
-        if current_room.value != 0: #si on trouve quelque chose, on retourne le chemin parcouru
-            return path 
-        else: #sinon on rajoute les pièces voisines
+        if current_room == goal_room: #si on trouve notre but, on arrête l'exploration
+            break
+        else: #sinon on rajoute les pièces voisines dans le file
             for i in current_room.nb_neighbors:
                 queue.append(current_room.neighbors[i])
-    return starting_room #si on sort de la boucle sans rien trouver on retourne la pièce actuelle
+    return path
 
 
-
-def a_star_search(starting_room,goal_room): #pas fini
+def a_star_search(starting_room, goal_room): #pas fini
     path = []
     queue = [starting_room]
-    cost=0
+    cost= abs(goal_room.x-starting_room.x) + abs(goal_room.x-starting_room.x)
     
     while not queue.empty():
         current_room = queue.pop()
         path.append(current_room)
-        cost= cost + 1 # + sqrt(xi-x)²+(yi-y)²
-        if current_room.value != 0:
+        cost = cost + 1 #poids uniforme pour chaque pièce 
+        if current_room == goal_room:
             break
         else:
             for i in current_room.nb_neighbors:
                 queue.append(current_room.neighbors[i])
                 #ordonnancer l'append en fonction d'un coût prédit sur l'heuristique
+                # => créer une nouvelle fnction comme append avec une notion de priorité ?
             
     return path
     
