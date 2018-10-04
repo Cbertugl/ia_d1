ef124d99844bc077066aa909ed13670427554dfd
cbertugl <camille.bertuglia@insa-cvl.fr>
2018-10-04 09:11:02 -0400

    refonte environnement (non testé)

--- environment.py
+++ environment.py
@@ -2,10 +2,29 @@ import random
 
 class Environment:
     
-    def __init__(self):
-        self.x, self.y = 10, 10; #cree un tableau 2D de 10 par 10
-        self.hmap= [[0 for i in range(self.x)] for j in range(self.y)] #initialise le tableau a 0
-        #on considere 0 = rien, 1 = poussiere, 2 = bijou, 3 = poussiere et bijou
+    def __init__(self,size):
+        self.x, self.y = size, size; #cree un tableau carré d'une taille fixée
+        for i in range(size): #on initialise les instances des pièces
+            for j in range(size):
+                self.hmap[i][j] = Room(i+1,j+1) #note: les pièces vont de 1 à size, le tableau hmap de 0 à size-1
+        for i in range(size): #on initialise les voisins des pièces
+            for j in range(size):
+                nb_neighbors = 0
+                if i > 0:
+                    if j > 0:
+                        neighbors.append(self.hmap[i-1][j-1])
+                        nb_neighbors++
+                    if j < size-1:
+                        neighbors.append(self.hmap[i-1][j+1])
+                        nb_neighbors++
+                if i < size-1:
+                    if j > 0:
+                        neighbors.append(self.hmap[i+1][j-1])
+                        nb_neighbors++
+                    if j < size-1:
+                        neighbors.append(self.hmap[i+1][j+1])
+                        nb_neighbors++
+                self.hmap[i][j].set_neighbors(neighbors,nb_neighbors)
         
     def gen(self):
         
@@ -25,13 +44,13 @@ class Environment:
         x = random.randint(0,9)
         y = random.randint(0,9)
             
-        if self.hmap[x][y] == 3:
+        if self.hmap[x][y].value == 3:
             pass #ne rien faire
                 
-        elif self.hmap[x][y] == 2:
-            self.hmap[x][y] = 3
+        elif self.hmap[x][y].value == 2:
+            self.hmap[x][y].value = 3
         else:
-            self.hmap[x][y] = 1
+            self.hmap[x][y].value = 1
         
         
     def genjewel(self):
@@ -39,17 +58,32 @@ class Environment:
         x = random.randint(0,9)
         y = random.randint(0,9)
         
-        if self.hmap[x][y] == 3:
+        if self.hmap[x][y].value == 3:
             pass #ne rien faire
             
-        elif self.hmap[x][y] == 1:
-            self.hmap[x][y] = 3
+        elif self.hmap[x][y].value == 1:
+            self.hmap[x][y].value = 3
             
         else:
-            self.hmap[x][y] = 2
+            self.hmap[x][y].value = 2
             
     def display(self):
         
         for i in range(self.x):
             for j in range(self.y):
-                print("Ligne ", i+1, "; Colonne ", j+1, "; Value ", self.hmap[i][j], ";")
+                print("Ligne ", i+1, "; Colonne ", j+1, "; Value ", self.hmap[i][j].value, ";")
+
+class Room:
+    def __init__(self,posx,posy):
+        self.x=posx
+        self.y=posy
+        self.nb_neighbors = 0
+        self.neighbors = []
+        self.value = 0 #on considere 0 = rien, 1 = poussiere, 2 = bijou, 3 = poussiere et bijou
+        
+    def set_neighbors(neighbors,nb_neighbors)
+        self.nb_neighbors=nb_neighbors
+        self.neighbors[neighbors[nb.neighbors-1] for i in range(nb_neighbors)]
+        
+        
+        
